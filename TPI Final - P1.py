90bb6120f03a1c588009264b4cb7eb1308e4cf8b
jeremiasjuarezREPO <jeremiasjuarez122@gmail.com>
2026-06-12 00:37:47 -0300

    Modificacion de funciones auxiliares y desarrollo de la funcion actualizar_datos_pais para la opcion 2

--- TPI Final - P1.py
+++ TPI Final - P1.py
@@ -26,24 +26,26 @@ def iniciar_programa():
 #Valida la opcion elegida por el usuario
         try:
 #Debe ser numero
-            opcion = int(opcion)
+            validar_opcion = int(opcion)
 #Debe estar entre 1 y 7
-            if opcion not in range(1,8):
+            if validar_opcion not in range( 1,8 ):
                 raise ValueError("Opción invalida: debe ingresar un número entre 1 y 7 ")
+            opcion = validar_opcion
 
         except ValueError as error:
-            if "invalid literal" in str(error):
+            if "invalid literal" in str( error ):
                 print("Dato ingresado invalido: debe ingresar un número entre 1 y 7")
             else:
                 print(error)
+            continue
 
 #Opcion 1 - Añadir pais a la base de datos
         if opcion == 1:
-            grabar_datos_nuevo_pais(datos)
+            grabar_datos_nuevo_pais( datos )
 
 #Opcion 2 - Actualizar datos de 1 pais
         elif opcion == 2:
-            print("opcion 2")
+            actualizar_datos_pais( datos )
 
 #Opcion 3 - Visualizar informacion sobre los paises ( muestra 1 o todos segun opte el usuario )
         elif opcion == 3:
@@ -109,7 +111,9 @@ def grabar_datos_nuevo_pais(datos):
     print("A continuación deberá ingresar: nombre, poblacion, superficie y continente al que pertenece el pais que desea añadir.\n")
 
 #Llamado de funciones auxiliares y guardado de su valor de retorno en las variables pertinentes 
-    nombre_nuevo_pais = validar_nombre_pais(datos)
+#La funcion validar_nombre_pais() recibe como 1er arg la lista con los dict de paises
+# y como 2do argumento la bandera en True para que evalue la existencia del nombre del pais dentro de los dict
+    nombre_nuevo_pais = validar_nombre_pais( datos, verificar_duplicado=True )
     poblacion_nuevo_pais = validar_poblacion_pais()
     superficie_nuevo_pais = validar_superficie_pais()
     continente_nuevo_pais = validar_continente_pais()
@@ -125,14 +129,18 @@ def grabar_datos_nuevo_pais(datos):
 #Emplea DictWriter ya que se estan almacenando los datos de los distintos paises en diccionarios dentro de la lista general "datos"
             escritor = csv.DictWriter( archivo, fieldnames=["nombre","poblacion","superficie","continente"])
 
-#Funcion writerow del objeto escritor que se creo en la linea anterior. Escribira los datos para el pais con la estructura de diccionario
-#Añadira una fila al final del archivo
-            escritor.writerow( {
+#Definicion de diccionario con los datos del pais a guardar
+            nuevo_pais = {
                 "nombre": nombre_nuevo_pais,
                 "poblacion": poblacion_nuevo_pais,
                 "superficie": superficie_nuevo_pais,
                 "continente": continente_nuevo_pais
-                } )
+                } 
+#Funcion writerow del objeto escritor que se creo en la linea anterior. Escribira los datos para el pais con la estructura de diccionario
+#Añadira una fila al final del archivo
+            escritor.writerow( nuevo_pais )
+#Actualizamos la lista datos presente en la memoria RAM. Logramos que "datos" sea igual al archivo .csv recien modificado
+            datos.append( nuevo_pais )
 
 #Manejo de errores especificos y previsibles. Si se dispara un error, se realizara el return para frenar la ejecucion de la funcion.
 #Manejo de error caso que no exista el archivo
@@ -160,8 +168,66 @@ def grabar_datos_nuevo_pais(datos):
     print(f"Superficie en kilometros cuadrados: { superficie_nuevo_pais }")
     print(f"Continente: { continente_nuevo_pais }\n")
 
-def actualizar_datos_pais():
-    print("Datos actualizados")
+#!Funcion para actualizar la informacion de un pais seleccionado por el usuario
+def actualizar_datos_pais( datos ):
+    print("A continuación deberá ingresar: nombre, poblacion y superficie del país que desea modificar.\n")
+
+#Valida y pide solo el nombre del país sin controlar duplicados ya que se hara mas adelante
+    nombre_pais_a_modificar = validar_nombre_pais( datos, verificar_duplicado=False )
+
+#Verifica que exista el pais que se quiere modificar
+    if not existe_pais_en_base_datos( nombre_pais_a_modificar ,datos ):
+        print("El país no se encuentra registrado. ")
+        return
+
+#Piden al usuario el dato por teclado y lo validan, guardan valor segun corresponda
+    nueva_poblacion_pais = validar_poblacion_pais()
+    nueva_superficie_pais = validar_superficie_pais()
+
+#Bucle for, iterara sobre lista datas, buscando el diccionario correspondiente al pais a modificar y actualiza los valores
+    for pais in datos:
+        if pais["nombre"] == nombre_pais_a_modificar:
+            pais["poblacion"] = nueva_poblacion_pais
+            pais["superficie"] = nueva_superficie_pais
+            break
+
+#Variable para almacenar la ruta del archivo a reescribir
+    ruta_archivo = "data/paises.csv"
+
+#Bloque try/except. Almacena la logica para la escritura del archivo y manejo de posibles errores
+    try:
+#Bloque de escritura iniciado con with para cierre seguro y automatico del archivo una vez terminada la tarea
+#Modo "w" para reescribir el archivo
+        with open( ruta_archivo, "w", newline="", encoding="utf-8") as archivo:
+
+#Emplea DictWriter ya que se estan almacenando los datos de los distintos paises en diccionarios dentro de la lista general "datos"
+            escritor = csv.DictWriter( archivo, fieldnames=["nombre","poblacion","superficie","continente"])
+#Volvemos aescribir los nombres de las columnas en el .csv
+            escritor.writeheader()
+
+#Funcion writerows del objeto escritor que se creo en lineas previas. Reescribira el archivo con los datos actualizados
+            escritor.writerows( datos )
+
+#Manejo de errores especificos y previsibles. Si se dispara un error, se realizara el return para frenar la ejecucion de la funcion.
+#Manejo de error caso que no exista el archivo
+    except FileNotFoundError:
+        print("Error: no se encontro el archivo en la ruta especificada")
+        return
+#Manejo de error caso que no se tengan permisos para editar el archivo o este abierto por otra aplicacion
+    except PermissionError:
+        print("Error: no posee permisos para la escritura del archivo. Verifique que no este siendo utilizado por otra aplicacion.")
+        return
+#Manejo de error caso que falle la codificacion
+    except UnicodeDecodeError:
+        print("Error: el formato de codificacion de caracteres no es compatible. No se pudo leer el archivo.")
+        return
+
+#Manejo de error generico ( re de seguridad ). Se manifestara para todos aquellos errores no contemplados previamente en la funcion.
+    except Exception as error:
+        print(f"Ocurrio un error inesperado: { type( error ).__name__} = { error }")
+        return
+    
+    print(f"Pais '{ nombre_pais_a_modificar }' actualizado correctamente.\n")
 
 def buscar_pais_nombre():
 #?Debe pedir nombre pais evaluarlo y mostrar por consola los datos para ese pais
@@ -200,8 +266,10 @@ def salir():
 #--Funciones auxiliares
 
 #**Funciones de validacion de inputs
-#Validacion de nombre de pais
-def validar_nombre_pais( datos ):
+#Validacion de nombre de pais. La funcion recibe 2 argumentos:
+#datos: es la lista que contiene los diccionarios con la informacion de los paises
+#verificar_duplicado: argumento a modo de bandera para determinar si se evalua o no la existencia del nombre del pais dentro de datos
+def validar_nombre_pais( datos, verificar_duplicado ):
 #Ciclo while para pedir el dato al usuario hasta que ingrese uno valido
     while True:
         try:
@@ -225,7 +293,9 @@ def validar_nombre_pais( datos ):
             elif not all( caracter.isalpha() or caracter.isspace() or caracter in "'-" for caracter in nuevo_pais ):
                 raise ValueError("El nombre del país no acepta caracteres especiales ( $, #, &, etc ).\n")
 #Busca en archivo cvs la existencia del pais y genera el error si la encuentra
-            elif existe_pais_en_base_datos( nuevo_pais, datos ):
+#Utiliza el valor del argumento "verificar_duplicado" solo si el flujo de ejecucion proviene de la invocacion de la funcion en la opcion 1
+#Para la opcion 2, "verificar_duplicado" sera False, por lo que no realizara esta evaluacion
+            elif verificar_duplicado and existe_pais_en_base_datos( nuevo_pais, datos ):
                 raise ValueError("El país ya existe en la base de datos. No se permiten duplicados.\n")
 
             return nuevo_pais
@@ -240,21 +310,21 @@ def validar_poblacion_pais():
     while True:
         try:
 
-            poblacion_nuevo_pais = input("Ingrese la población del país que desea añadir: ").strip()
+            poblacion_pais = input("Ingrese la población del país: ").strip()
 #Valida que se hay ingresado algo por teclado
-            if len(poblacion_nuevo_pais) == 0:
+            if len(poblacion_pais) == 0:
                 raise ValueError("Debe ingresar un dato numerico válido para la población del país.\n")
 #Verifica que se este ingresando un numero entero
-            elif not poblacion_nuevo_pais.isdigit():
+            elif not poblacion_pais.isdigit():
                 raise ValueError("Dato ingresado inválido. Debe ingresar un número entero mayor o igual a 1.\n")
 #Verifica que la poblacion sea al menos 1
-            elif int(poblacion_nuevo_pais) < 1:
+            elif int(poblacion_pais) < 1:
                 raise ValueError("La población de un país no puede ser un número inferior a uno\n")
 #Impone limite de poblacion para el pais. evita que se ingresen numeros excesivos o absurdos )
-            elif int(poblacion_nuevo_pais) > 2000000000:
+            elif int(poblacion_pais) > 2000000000:
                 raise ValueError("La población del país no puede superar los dos mil millones de habitantes\n")
 
-            return poblacion_nuevo_pais
+            return poblacion_pais
         
         except ValueError as error:
             print(error)
@@ -266,21 +336,22 @@ def validar_superficie_pais():
     while True:
         try:
 
-            superficie_nuevo_pais = input("Ingrese la superficie en kilometros cuadrados del país que desea añadir: ").strip()
-#Verifica que se hay ingresado algo por teclado
-            if len(superficie_nuevo_pais) == 0:
+            superficie_pais = input("Ingrese la superficie en kilometros cuadrados del país: ").strip()
+#Verifica que se haya ingresado algo por teclado
+            if len(superficie_pais) == 0:
                 raise ValueError("Debe ingresar un dato numerico válido para la superficie del país.\n")
 
-#Verifica que la superficie en km2 sea un numero. Implementa float porque puede ser un numero con decimales ( ej: Ciudad del Vaticano 0.44 mk2 )
+#Verifica que la superficie en km2 sea un numero. 
+#Implementa float porque puede ser un numero con decimales ( ej: Ciudad del Vaticano 0.44 mk2 )
             try:
-                numero = float(superficie_nuevo_pais)
+                numero = float(superficie_pais)
             except ValueError:
                 raise ValueError("Dato ingresado inválido. Debe ingresar un número para este campo.\n")
-#La superficie en km2 no puede ser menor o igual a cero. Emite error
             if numero <= 0:
                 raise ValueError("La superficie en kilometros cuadrados del país debe ser un valor mayor a cero\n")
+#La superficie en km2 no puede ser menor o igual a cero. Emite error
 
-            return superficie_nuevo_pais
+            return superficie_pais
         
         except ValueError as error:
             print( error )
@@ -313,7 +384,9 @@ def validar_continente_pais():
         except ValueError as error :
             print( error )
 
-#Verifica si existen coincidencias de nombres en el archivo csv
+#Verifica si existen coincidencias de nombres en el archivo csv. Recibe 2 argumentos
+#pais: es el nombre del pais que ingreso el usuario
+#datos: es la lista que contiene los diccionarios con la info de los paises
 def existe_pais_en_base_datos( pais, datos ):
 
 #Variable bandera, si se encuentra coincidencia entre nombres de paises cambia a true y se retorna al final de la funcion
